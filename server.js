b1ea6115539e1db585a95d3a5a0a78f7e18e655c
anandhu <anandhu.mohan@chaipoint.com>
2016-12-12 01:36:22 +0530

    messaging done

--- server.js
+++ server.js
@@ -13,3 +13,16 @@ console.log("server runnning");
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/index.html');
 });
+
+//open a connection with socket.io
+io.sockets.on('connection',function(socket){
+	connections.push(socket);
+	console.log("connection created");
+
+	
+
+
+	socket.on('send message',function(data){
+	io.sockets.emit('new message',{msg:data});
+	});
+});
