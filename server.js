b5f96f8f7f2ac86d1d023f8169cce8e5af1ff437
anandhumohan <anandhumohan5@gmail.com>
2016-12-13 12:22:39 +0530

    final

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var io = require("socket.io").listen(server);
 
 //maining two arrays for users and connections
 
-user = [];
+users = {};
 connections = [];
 server.listen(process.env.port || 3000);
 console.log("server runnning");
@@ -23,19 +23,45 @@ io.sockets.on('connection',function(socket){
 
 
 	socket.on('send message',function(data){
-	io.sockets.emit('new message',{msg:data});
+		var msg = data.trim();
+		if(msg.substr(0,1) === '@'){
+			msg = msg.substr(1);
+			var index = msg.indexOf(' ');
+			if(index !== -1){
+				var name = msg.substring(0,index); 
+				var msg = msg.substring(index + 1);
+				if(name in users){
+					users[name].emit('new again',{msg:msg});
+				}
+			}
+			else{
+				callback("format error");
+			}
+		}
+		else{
+		io.sockets.emit('new message',{msg:msg});
+		}
 	});
 
 	socket.on('new user',function(data, callback){
 	//io.sockets.emit('new user',{msg:data});
-	callback(true);
-	socket.username = data;
-	users.push(socket.username);
-	updateUser(); 
+	//callback(true);
+	//socket.username = data;
+	//users.push(socket.username);
+	if(data in users){
+		callback(false);
+	}
+	else{
+		callback(true);
+		socket.username = data;
+		users[socket.username] = socket;
+		updateUser();
+	}
+	 
 
 	});
 	function updateUser(){
-		io.sockets.emit('get users',users);
+		io.sockets.emit('get users',Object.keys(users));
 	}
 
 });
